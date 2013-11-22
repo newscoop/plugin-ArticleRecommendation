98cd01aebeb9ead136612e26dabbfed90e9bb342
Rafał Muszyński <rmuszynski1@gmail.com>
2013-11-22 09:54:19 +0100

    backend setup

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -8,6 +8,11 @@
 
 namespace Newscoop\ArticleRecommendationBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
 class AdminController extends Controller
 {
     /**
